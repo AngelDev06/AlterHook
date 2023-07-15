a5ed1cfa2df3a5a2946d9944cf15118c73760e8d
AngelDev06 <aggelos.tserkeza@gmail.com>
2023-07-15 21:59:28 +0300

    Added template string utility and utils.h

---
+++ src/headers/utilities/utils.h
@@ -0,0 +1,14 @@
+/* Part of the AlterHook project */
+/* Designed & implemented by AngelDev06 */
+#pragma once
+#include <cstddef>
+#include "utils_macros.h"
+#if !utils_cpp17
+#error unsupported c++ version (at least c++17 is needed)
+#endif
+#include "other.h"
+#include "type_sequence.h"
+#include "calling_conventions.h"
+#include "function_traits.h"
+#include "static_vector.h"
+#include "template_string.h"
