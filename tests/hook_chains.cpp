6800a8834420e122470e2f8df6311b416f032603
AngelDev06 <aggelos.tserkeza@gmail.com>
2023-10-20 22:28:49 +0300

    Added more checks for `hook_chain` (especially ones involving splice methods) and fixed bugs

--- tests/hook_chains.cpp
+++ tests/hook_chains.cpp
@@ -82,4 +82,80 @@ int main()
                     func_called::detourcls_func6, func_called::detourcls_func4);
   assert(std::tie(instance.x, instance.y, instance.z) == origresult);
   std::cout << "-------------------------------------------\n";
+
+  chain1.push_front(&detourcls::func7, original7);
+  instance.func();
+  verify_call_stack(func_called::originalcls_func, func_called::detourcls_func7,
+                    func_called::detourcls_func5, func_called::detourcls_func6,
+                    func_called::detourcls_func4);
+  assert(std::tie(instance.x, instance.y, instance.z) == origresult);
+  std::cout << "-------------------------------------------\n";
+
+  chain1.erase(std::next(chain1.begin()),
+               std::prev(chain1.eend(), 2)->get_iterator());
+  assert(chain1.size() == 3);
+  instance.func();
+  verify_call_stack(func_called::originalcls_func, func_called::detourcls_func7,
+                    func_called::detourcls_func6, func_called::detourcls_func4);
+  assert(std::tie(instance.x, instance.y, instance.z) == origresult);
+  std::cout << "-------------------------------------------\n";
+
+  chain1.push_back(&detourcls::func8, original8, false);
+  chain1.push_back(&detourcls::func3, original3);
+  instance.func();
+  verify_call_stack(func_called::originalcls_func, func_called::detourcls_func7,
+                    func_called::detourcls_func6, func_called::detourcls_func4,
+                    func_called::detourcls_func3);
+  assert(std::tie(instance.x, instance.y, instance.z) == origresult);
+  std::cout << "-------------------------------------------\n";
+
+  chain1.erase(std::prev(chain1.eend(), 2), chain1.eend());
+  assert(chain1.size() == 3);
+  instance.func();
+  verify_call_stack(func_called::originalcls_func, func_called::detourcls_func7,
+                    func_called::detourcls_func6);
+  assert(std::tie(instance.x, instance.y, instance.z) == origresult);
+  std::cout << "-------------------------------------------\n";
+
+  alterhook::hook_chain chain2{
+    &originalcls::func2, std::make_pair(&detourcls::func, std::ref(original)),
+    std::make_pair(&detourcls::func2, std::ref(original2)),
+    std::make_pair(&detourcls::func3, std::ref(original3))
+  };
+  instance.func2();
+  verify_call_stack(func_called::originalcls_func2, func_called::detourcls_func,
+                    func_called::detourcls_func2, func_called::detourcls_func3);
+  assert(std::tie(instance.x, instance.y, instance.z) == origresult);
+  std::cout << "-------------------------------------------\n";
+
+  chain2.splice(std::next(chain2.begin()), chain1, chain1.ebegin(),
+                chain1.eend());
+  assert(chain2.size() == 5);
+  assert(chain1.size() == 1);
+  instance.func2();
+  verify_call_stack(func_called::originalcls_func2, func_called::detourcls_func,
+                    func_called::detourcls_func7, func_called::detourcls_func6,
+                    func_called::detourcls_func2, func_called::detourcls_func3);
+  assert(std::tie(instance.x, instance.y, instance.z) == origresult);
+  std::cout << "-------------------------------------------\n";
+
+  chain2.swap(std::next(chain2.ebegin()), chain1, chain1.begin());
+  assert(chain2.size() == 5);
+  assert(chain1.size() == 1);
+  instance.func2();
+  verify_call_stack(func_called::originalcls_func2, func_called::detourcls_func,
+                    func_called::detourcls_func8, func_called::detourcls_func6,
+                    func_called::detourcls_func2, func_called::detourcls_func3);
+  assert(std::tie(instance.x, instance.y, instance.z) == origresult);
+  std::cout << "-------------------------------------------\n";
+
+  chain2.merge(chain1);
+  assert(chain2.size() == 6);
+  assert(chain1.empty());
+  instance.func2();
+  verify_call_stack(func_called::originalcls_func2, func_called::detourcls_func,
+                    func_called::detourcls_func8, func_called::detourcls_func6,
+                    func_called::detourcls_func2, func_called::detourcls_func3);
+  assert(std::tie(instance.x, instance.y, instance.z) == origresult);
+  std::cout << "-------------------------------------------\n";
 }
